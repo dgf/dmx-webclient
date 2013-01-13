001271a34be415e5b655bcafa64fe816e3ca65d6
dgf <github@dagnu.de>
2013-01-13 13:04:16 +0100

    move ace vendor code

--- src/main/resources/web/script/plugin.js
+++ src/main/resources/web/script/plugin.js
@@ -1,5 +1,5 @@
 dm4c.add_plugin('dmx.plugin', function () {
-    dm4c.load_script("/org.digitalmemex.dmx-webclient/script/vendor/ace/ace.js")
+    dm4c.load_script("/org.digitalmemex.dmx-webclient/script/vendor/ace.js")
     dm4c.load_script("/org.digitalmemex.dmx-webclient/script/vendor/ace/mode-javascript.js")
     dm4c.load_script("/org.digitalmemex.dmx-webclient/script/vendor/ace/theme-eclipse.js")
 })
