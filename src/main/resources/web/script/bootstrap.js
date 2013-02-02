d509ac0017f23fa969430908ade2272573969d44
dgf <github@dagnu.de>
2013-02-02 10:02:35 +0100

    require d3 wrap

--- src/main/resources/web/script/bootstrap.js
+++ src/main/resources/web/script/bootstrap.js
@@ -8,9 +8,13 @@ define('dm4rest', ['/de.deepamehta.webclient/script/util/rest_client.js'], funct
     return new RESTClient('/core') // wrap DeepaMehta 4 REST client utility
 })
 
+define('d3', ['/org.digitalmemex.dmx-webclient/script/vendor/d3.min.js'], function () {
+    return d3 // wrap d3
+})
+
 requirejs.config({ baseUrl: '/org.digitalmemex.dmx-webclient/script/vendor' })
-requirejs(['jQuery', 'knockout', 'domReady!', 'cs', 'jade', 'css', 'lessc', 'less'],
-function ($) { // knockout and require plugins ready
+requirejs(['jQuery', 'd3', 'knockout', 'domReady!', 'cs', 'jade', 'css', 'lessc', 'less'],
+function ($) { // d3, knockout and require plugins ready
     requirejs.config({ baseUrl: '/dmx/script' })
     $(function() { // jQuery ready => start up
         var $dmx = $('#dmx')
